0a7621e5b7009a27b1328237aba7db8329b713d3
JermaineMckenna <jermaine.mckenna_@outlook.com>
2025-11-17 12:50:42 +0000

    fixed image issues through json file, uploaded 10 images to products and static folder,

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from products.models import Product
 
 def index(request):
-    return render(request, 'home/index.html')
+    trending = Product.objects.all().order_by('-id')[:4]  # newest products
+
+    return render(request, "home/index.html", {
+        "trending": trending
+    })
+
